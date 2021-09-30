f664aa0f237f8fc1f17707915a8c69a8cf30f1c8
nal95 <alex.nguepi@yahoo.fr>
2021-09-30 15:18:44 +0200

    nft mint form

--- Wine_NFTs_Dapp/main.js
+++ Wine_NFTs_Dapp/main.js
@@ -2,12 +2,11 @@
 Moralis.initialize("WtIouaRR03pa2oPYIJxisLd1VFPiLNBLsLxVD0io");
 Moralis.serverURL = "https://ezmfxliqvfeo.moralishost.com:2053/server";
 
-/*let homepage = "http://127.0.0.1:5500/index.html";
 
-if (Moralis.User.current() == null && window.location.href != homepage) {
-  document.querySelector("body").style.display = "none";
-  window.location.href = "index.html";
-}*/
+
+const nft_contract_address = "0xbFEf1ADC73Ed3Ed9EDa674FB7E0A9503E7Ff5354"; //NFT Minting Contract Use This One "Batteries Included", code of this contract is in the github repository under contract_base for your reference.
+
+
 
 login = async () => {
   await Moralis.Web3.authenticate().then(async function (user) {
@@ -19,9 +18,42 @@ login = async () => {
   });
 };
 
+/*async function upload() {
+  const fileInput = document.getElementById("file");
+  const data = fileInput.files[0];
+  const imageFile = new Moralis.File(data.name, data);
+
+  document.querySelector("#SN").disabled = true;
+  document.querySelector("#upload").disabled = true;
+  document.querySelector("#file").disabled = true;
+  document.querySelector("#name").disabled = true;
+  document.querySelector("#description").disabled = true;
+  await imageFile.saveIPFS();
+  const imageURI = imageFile.ipfs();
+  const metadata = {
+    name: document.getElementById("name").value,
+    description: document.getElementById("description").value,
+    image: imageURI,
+  };
+  let _SN = document.getElementById("SN").value;
+  const metadataFile = new Moralis.File("metadata.json", {
+    base64: btoa(JSON.stringify(metadata)),
+  });
+  await metadataFile.saveIPFS();
+  const metadataURI = metadataFile.ipfs();
+  return mintToken(document.getElementById("SN").value, metadataURI).then(
+    notify
+  );
+}*/
+
+logout = async () => {
+  await Moralis.User.logOut();
+  window.location.href = "index.html";
+};
+
 if (document.querySelector("#btn-login") != null) {
   document.querySelector("#btn-login").onclick = login;
 }
-//get-transactions-link
-//get-balances-link
-//get-nfts-link
+if (document.querySelector("#btn-logout") != null) {
+  document.querySelector("#btn-logout").onclick = logout;
+}
