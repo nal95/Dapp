546b13753fab6c8fc664e0957867b5242adc5ae7
nal95 <alex.nguepi@yahoo.fr>
2021-10-04 11:35:42 +0200

    lager smart contract

--- Wine_NFTs_Dapp/AppJS/NFTs.js
+++ Wine_NFTs_Dapp/AppJS/NFTs.js
@@ -1,6 +1,6 @@
 // connect to Moralis server
-Moralis.initialize("U8pWrvTB4I6vpnKF24fyZxrD4YQue5wHnuaHtVPR");
-Moralis.serverURL = "https://oyoj4lpcodao.moralishost.com:2053/server";
+Moralis.initialize("LxM62hGGsVUGycHbQ4xqgkZwvljKOlhxYDD40o4i");
+Moralis.serverURL = "https://gatgq3cu6b0i.moralishost.com:2053/server";
 let web3;
 
 const nft_user_address = "0xC451a31d586Fa8c91c382f4a7800ac318D966762"; //NFT Minting Contract Use This One "Batteries Included", code of this contract is in the github repository under contract_base for your reference.
@@ -11,15 +11,15 @@ async function init() {
 }
 
 init();
-getNFTs = async () => {
-  console.log("cool connected to nft fiel");
-  let nfts = await Moralis.Web3API.account.getNFTs(options);
-  //console.log(nfts);
-  let tableOfNFTs = document.querySelector("#tableOfNFTs");
 
+getNFTs = async () => {
+  //console.log("cool connected to nft fiel");
+  const nfts = await Moralis.Web3API.account.getNFTs(options);
+  //console.log(nfts);
+  // let tableOfNFTs = document.querySelector("#tableOfNFTs");
   if (nfts.result.length > 0) {
     nfts.result.forEach((n) => {
-      console.log(JSON.parse(n.metadata));
+      //console.log(JSON.parse(n.metadata));
       let metadata = JSON.parse(n.metadata);
       let content = `
       <div class="card col-md-3">
@@ -34,15 +34,5 @@ getNFTs = async () => {
     });
   }
 };
-
-/*fixURL = (url) => {
-  if (url.startsWith("https://ipfs")) {
-    return (
-      "https://ipfs.moralis.io:2053/ipfs/" + url.split("ipfs://").slice(-1)
-    );
-  } else {
-    return url + "?format=json";
-  }
-};*/
 
 getNFTs();
--- Wine_NFTs_Dapp/contracts/Lager.sol
+++ Wine_NFTs_Dapp/contracts/Lager.sol
@@ -1,10 +1,18 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: GPL-3.0
+
 pragma solidity >=0.8.0;
 
 contract Lager{
-
     
+
     address admin = msg.sender;
+    
+    struct Check {
+        string serialNummer;
+        uint256 date;
+    }
+    
+    Check[] public check;
 
 
     
@@ -14,24 +22,65 @@ contract Lager{
     
     // event qui nous permettra d'obtenir nos Hash et le temps 
     event Store (uint date, bytes32 Hash);
-    event checkInEvent(uint date,string _SN, string tokenURI);
-    event checkOutEvent(uint date, string SN);
-    
+    event checkIOEbvent(uint date, string);
     //la fonction qui sera appeler pour sauvegarder les Hash
     function storageData ( 
-        string memory _timestamp,
-        string memory _temperature,
-        string memory _humidity) external onlyAdmin returns (bytes32 ){
-        filehash[admin] = keccak256(abi.encodePacked(_timestamp,_temperature,_humidity));
+        string memory Hash
+        )external onlyAdmin returns (bytes32 ){
+        filehash[admin] = keccak256(abi.encodePacked(Hash));
         emit Store(block.timestamp, filehash[admin]);
         return filehash[admin];
     }
     
-            
+    function hashCompareWithLengthCheck(string memory a, string memory b) internal pure returns (bool) {
+        if(bytes(a).length != bytes(b).length) {
+            return false;
+        } else {
+            return keccak256(abi.encodePacked(a)) == keccak256(abi.encodePacked(b));
+        }
+    }
+    
+    function exist(string memory _str) internal view returns(bool ){
+      bool setter  = false;
+      for(uint256 i = 0; i< check.length; i++){
+          string memory _SNN = check[i].serialNummer;
+          if(hashCompareWithLengthCheck(_str,_SNN) ){
+              setter = true;
+          }
+      }
+      
+      return setter;
+    }
+  
+    
+    function checkIn(string memory _str) public onlyAdmin returns (uint256){
+        require( exist(_str) == false,"serialNummer allredy exist");
+        check.push(Check(_str,block.timestamp));
+        emit checkIOEbvent (block.timestamp, _str);
+        return (block.timestamp);
+        
+       
+    }
+    
+    function checkOut(string memory _str) public onlyAdmin returns (uint256, bool){
+        uint256 during = 0 ;
+        bool helper = false;
+        for(uint256 i=0; i<check.length; i++){
+            if(hashCompareWithLengthCheck( check[i].serialNummer, _str )){
+                during = block.timestamp - check[i].date;
+                for (uint256 j=i; j<check.length-1; j++){
+                    check[j]=check[j+1];
+                }
+                check.pop();
+                helper = true;
+            }
+        }
+        emit checkIOEbvent (during, _str);
+        return (during , helper);
+    }
+          
     modifier onlyAdmin {
-      require(msg.sender == admin);
+      require(msg.sender == admin, "you are not the manager of this action");
       _;
    }
-
-
 }
